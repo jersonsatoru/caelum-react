7a0ec985f3045da87dfc102575b4c560c324b70f
Jerson Satoru Uyekita <jersonsatoru@yahoo.com.br>
2019-08-17 16:53:20 -0300

    Refactore Cabecalho and Add message when user has no tweet

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './cabecalho.css'
-// import './navMenu.css'
 
-class Cabecalho extends Component {
-    render() {
-        return (
-            <header className="cabecalho">
-                <div className="cabecalho__container container">
-                    <h1 className="cabecalho__logo">
-                        <a href="/">Twitelum</a>
-                    </h1>
-                    { this.props.children }
-                </div>
-            </header>
-        )
-    }
+function Cabecalho({ children }) {
+    return (
+        <header className="cabecalho">
+            <div className="cabecalho__container container">
+                <h1 className="cabecalho__logo">
+                    <a href="/">Twitelum</a>
+                </h1>
+                { children }
+            </div>
+        </header>
+    )
 }
 
-export default Cabecalho
+export default Cabecalho;
